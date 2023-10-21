b0e03d8a38ad1222334d0adda028527d8022f6a4
Ashwani Pandey <110717013+ashwanipydev@users.noreply.github.com>
2023-10-21 05:40:40 +0000

    problem 8

---
+++ 06_Day6/index.js
@@ -0,0 +1,12 @@
+//Problem 8
+/**
+ * Write a functon that calculates and prints the sum of the digits of a given numbers?
+ * Input: A number, positive integers -> 12,34, 123, 1234
+ * Return: sum of all digits
+ */
+
+function sumOfAllDigits(digits){
+    
+}
+
+sumOfAllDigits(1234)
