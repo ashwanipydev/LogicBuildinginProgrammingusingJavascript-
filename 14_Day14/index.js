b2f6e53bb0dec969cc7cf113c17b7e1267b87d36
ashwani <pydev.ashwani@gmail.com>
2023-10-27 10:26:31 +0530

    simple Interest

---
+++ 14_Day14/index.js
@@ -0,0 +1,27 @@
+//!  Problem statement
+/**
+ * *Write a function that calculates and prints the simple interest on a loan amount.
+ * *Input:
+ * ------ Principal amount
+ * ------ Interest rate(yearly)
+ * ------ Time duration (how many years)
+ * *Return: Interest Amount
+ * *Simple interest:
+ * * (Principal * Interest Rate * Time Duration) / 100
+ */
+
+function calculatesSimpleInterest(pa, ir, td){
+    let si = 0
+    if(pa > 1 && ir > 1 && td >= 1){
+        si = (pa * ir * td)/100
+    }else{
+        throw new Error('Erron 401')
+    }
+    return si
+}
+
+
+console.log(calculatesSimpleInterest(1000.50,5.75,1.5));
+console.log(calculatesSimpleInterest(20000,10,5));
+
+
