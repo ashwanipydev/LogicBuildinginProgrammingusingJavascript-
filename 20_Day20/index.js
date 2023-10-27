004d587327166fadd48caf01e4553a2cc3185045
ashwani <pydev.ashwani@gmail.com>
2023-10-27 13:55:13 +0530

    isArmstrong

---
+++ 20_Day20/index.js
@@ -0,0 +1,15 @@
+//? Problem statement - Fizz Buzz
+/**
+ * *Write a program that prints prints numbers from 1 to N replacing
+ *  ?multiples of 3 with "Fizz"
+ *  ?multiples of 5 with "Buzz"
+ *  ?multiples of 3 with "FizzBuzz"
+ */
+
+function fizzBuzz(num){
+    for(const n of num){
+        console.log(n);
+    }
+}
+
+fizzBuzz(5)
